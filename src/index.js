107d9655bf98abdb1603c657ff4fe565287aac11
Nastya125 <nastyatrue@mail.ru>
2022-11-13 14:16:57 +0300

    1 commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/no-unresolved
+import './css/style.css';
+
+import './js/app';
---
+++ src/js/tests/app.test.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-undef */
+import Character from '../Character';
+import Magic from '../Magic';
+import Infantry from '../Infantry';
+import Archer from '../Archer';
+
+// eslint-disable-next-line no-undef
+test('Верный ввод лучник', () => {
+  const received = new Archer('Ron', 'Bowman');
+  const expected = {
+    name: 'Ron',
+    type: 'Bowman',
+    health: 100,
+    level: 1,
+    attack: 25,
+    defence: 25,
+  };
+  // eslint-disable-next-line no-undef
+  expect(received).toEqual(expected);
+});
+
+// eslint-disable-next-line no-undef
+test('Верный ввод маг', () => {
+  const received = new Magic('Ron', 'Magician');
+  const expected = {
+    name: 'Ron',
+    type: 'Magician',
+    health: 100,
+    level: 1,
+    attack: 10,
+    defence: 40,
+  };
+  expect(received).toEqual(expected);
+});
+
+test('Верный ввод пехота', () => {
+  const received = new Infantry('Ron', 'Swordsman');
+  const expected = {
+    name: 'Ron',
+    type: 'Swordsman',
+    health: 100,
+    level: 1,
+    attack: 40,
+    defence: 10,
+  };
+  expect(received).toEqual(expected);
+});
+
+test('Не верное имя', () => {
+  expect(() => new Character('R', 'Undead')).toThrow();
+});
+
+test('Не верный тип лучник', () => {
+  expect(() => new Archer('Ron', 'Soldier')).toThrow();
+});
+
+test('Не верный тип маг', () => {
+  expect(() => new Magic('Ron', 'Soldier')).toThrow();
+});
+
+test('Не верный тип пехота', () => {
+  expect(() => new Infantry('Ron', 'Soldier')).toThrow();
+});
